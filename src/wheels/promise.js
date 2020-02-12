790712c9c9a4f5aea41b55a60de575c4a08a7547
罗坤 <1498710037@qq.com>
2020-02-12 16:34:58 +0800

    Chrome Promise

--- src/wheels/promise.js
+++ src/wheels/promise.js
@@ -1,26 +1,93 @@
+'use strict';
 
 /**
- * 规范：Follow by [Promise/A+](https://promisesaplus.com/)
+ * <del> 规范：Follow by [Promise/A+](https://promisesaplus.com/) </del>
+ * 规范： Follow by chrome console log
  * 参考答案： https://github.com/forthealllight/blog/issues/4
  * **/
 
 // 2. Requirements|要求
 // 2.1 Promise States
-const PENDING = 1;
-const FULFILLED = 2;
-const REJECTED = 3;
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
 
-function Promise(){
+const PromiseStatus = Symbol("[[PromiseStatus]]");
+const PromiseValue = Symbol("[[PromiseValue]]");
+let listenersMap = new WeakMap();
+let resolve,reject;
+let catchFunction = function(){};
 
+function Promise2(){
+    // 一个promise必须有3个状态，pending，fulfilled(resolved)，rejected
+    // 当处于pending状态的时候，可以转移到fulfilled(resolved)或者rejected状态。
+    // 当处于fulfilled(resolved)状态或者rejected状态的时候，就不可变。
+
+    listenersMap.set(this, []);
+    this[PromiseStatus] = PENDING;
+    resolve =(res)=>{
+        this[PromiseStatus] = FULFILLED;
+        this[PromiseValue] = res;
+
+        // dispatch listener
+        let current = listenersMap.get(this).shift()||[];
+        this.then(...current)
+
+    };
+
+    reject =(err)=>{
+        this[PromiseStatus] = REJECTED;
+        !this[PromiseValue] && (this[PromiseValue] = err);
+
+        listenersMap.set(this, []);
+        catchFunction(err)
+    };
+
+    try{
+        arguments[0](resolve,reject)
+    }catch(err){
+        reject(err)
+    }
 }
 
-Promise.prototype.then = function(onFulfilled, onRejected){
-    if(typeof onFulfilled !== "function" || typeof onRejected !== "function"){
-        new TypeError("Both onFulfilled and onRejected are optional arguments:")
+Promise2.prototype.then = function(){
+    let onFulfilled=typeof arguments[0]==="function"?arguments[0]:function(x){return x},
+        onRejected = typeof arguments[1] === "function" ? arguments[1] : function(e){throw e};
+
+    let listeners = listenersMap.get(this);
+
+    if(this[PromiseStatus] === PENDING){
+        listeners.push([onFulfilled,onRejected]);
+    }else if(this[PromiseStatus] === FULFILLED){
+        try{
+            onFulfilled(this[PromiseValue]);
+            listeners.length > 0 && resolve(this[PromiseValue])
+        }catch (e) {
+            reject(e)
+        }
+
+    }else if(this[PromiseStatus] === REJECTED){
+        onRejected()
     }
 
     // then must return a promise [3.3].
     return this
 };
 
-module.exports = Promise;
+Promise2.prototype.catch = function(){
+    catchFunction = (err)=>{
+        arguments[0](err)
+    }
+};
+
+// for promises-aplus-tests
+Promise2.deferred=function(){
+    let dfd={};
+    dfd.promise=new Promise2(function(resolve,reject){
+        dfd.resolve=resolve;
+        dfd.reject=reject;
+    });
+    return dfd;
+};
+
+module.exports = Promise2;
--- test/promise.test.js
+++ test/promise.test.js
@@ -4,7 +4,7 @@ const expect = require('chai').expect;
 
 // Follow the Promise/A+ promises-tests
 
-describe('promise', function(){
+describe('Promise', function(){
     // 1."promise" is an object or function with a then method whose behavior conforms to this specification.
     let promise = new Promise();
 
@@ -16,6 +16,9 @@ describe('promise', function(){
         expect(promise.then).to.be.a('function');
     });
 
+    // assert(promise.then() instanceof promise,
+    //     "\"promise\" 的`then`方法必须返回一个promise");
+
     // 2."thenable" is an object or function that defines a then method.
     let thenable = {
         then:function(){
@@ -24,3 +27,40 @@ describe('promise', function(){
     }
 });
 
+describe('promise then', function(){
+    let date = new Date().getTime();
+    let time = 1000;
+    let promise = new Promise(function(resolve, reject){
+       setTimeout(()=>{
+           resolve("1秒之后执行")
+       },time)
+   });
+
+   promise.then(res=>{
+       assert(new Date().getTime() - date > time && new Date().getTime() - date < time*2,"应在\"1秒之后执行\"")
+   });
+
+});
+
+describe('promise then & catch', function(){
+    let date = new Date().getTime();
+    let time = 1000;
+    let promise = new Promise(function(resolve, reject){
+        setTimeout(()=>{
+            resolve("1秒之后执行")
+        },time)
+    });
+
+    let error = new Error("error");
+
+    promise.then(res=>{
+        assert(new Date().getTime() - date > time && new Date().getTime() - date < time*2,"应在\"1秒之后执行\"");
+
+        throw error;
+    }).then(res=>{
+        assert.fail("如果有一个onFulfilled或onRejected返回一个值x，请运行Promise Resolution Procedure [[Resolve]](promise2, x)。");
+    }).catch(err=>{
+        assert(err === error,"如果任何一个onFulfilled或onRejected引发异常e，则promise2必须以拒绝e为理由。");
+    });
+
+});
